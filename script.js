bd3e9915c68a85deb8fbd40da162b8847fe0a263
Arun Kumar R <aruntutter19@gmail.com>
2023-11-18 20:46:51 +0530

    updated again with audio and audio UI

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ function toggleFlap(event) {
 function redirectToGitHub() {
   window.location.href = "https://ephemeral-flan-a16124.netlify.app/";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const audio = document.querySelector(".custom-audio audio");
+  const playPauseBtn = document.querySelector(".play-pause-btn");
+  const progressBar = document.querySelector(".progress-bar");
+  const progressIndicator = document.querySelector(".progress-indicator");
+
+  let isPlaying = false;
+
+  playPauseBtn.addEventListener("click", function () {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    isPlaying = !isPlaying;
+  });
+
+  audio.addEventListener("timeupdate", function () {
+    const progress = (audio.currentTime / audio.duration) * 100;
+    progressIndicator.style.width = progress + "%";
+  });
+
+  progressBar.addEventListener("click", function (e) {
+    const pos =
+      (e.clientX - progressBar.getBoundingClientRect().left) /
+      progressBar.clientWidth;
+    audio.currentTime = pos * audio.duration;
+  });
+});
